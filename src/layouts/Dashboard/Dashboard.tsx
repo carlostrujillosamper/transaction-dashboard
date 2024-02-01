6a3370cb87caba29a54629f2c8c1a021f19a6acf
carlostrujillosamper <carlostsdvnyc@gmail.com>
2024-02-01 18:14:43 +0100

    feat(Dashboard): Add spinner for loading

--- src/layouts/Dashboard/Dashboard.tsx
+++ src/layouts/Dashboard/Dashboard.tsx
@@ -1,4 +1,4 @@
-import { Box, Heading, Select, Text, VStack } from "@chakra-ui/react";
+import { Box, Heading, Select, Spinner, Text, VStack } from "@chakra-ui/react";
 import { TransactionTable } from "../../components/Transactions/TransactionTable/TransactionTable";
 import { HttpError } from "../../components/common/HTTPError/HTTPError";
 import { useDashboardContext } from "./useDashboardContext";
@@ -7,43 +7,38 @@ export function Dashboard() {
   const { error, isLoading, setClientId } = useDashboardContext();
   return (
     <>
-        <VStack>
-          <Box w="100%">
-            <Heading as="h1" size="xl" textAlign="left">
-              Dashboard
-            </Heading>
-            <Text
-              fontWeight={200}
-              fontSize="25px"
-              textAlign="left"
-              color="grey"
-            >
-              Transaction List
-            </Text>
-          </Box>
-          <Box
-            border="1px solid #F4F8FA"
-            rounded={5}
-            boxShadow="md"
-            w="100%"
-            padding={4}
-            bgColor="#F4F8FA"
-            marginBottom="2%"
+      <VStack>
+        <Box w="100%">
+          <Heading as="h1" size="xl" textAlign="left">
+            Dashboard
+          </Heading>
+          <Text fontWeight={200} fontSize="25px" textAlign="left" color="grey">
+            Transaction List
+          </Text>
+        </Box>
+        <Box
+          border="1px solid #F4F8FA"
+          rounded={5}
+          boxShadow="md"
+          w="100%"
+          padding={4}
+          bgColor="#F4F8FA"
+          marginBottom="2%"
+        >
+          <Select
+            placeholder="Select Client"
+            w="20%"
+            bgColor="white"
+            name="client"
+            onChange={(e) => setClientId(e.target.value)}
           >
-              <Select
-                placeholder="Select Client"
-                w="20%"
-                bgColor="white"
-                name="client"
-                onChange={(e) => setClientId(e.target.value)}
-              >
-                <option value="option1">Client 1</option>
-                <option value="option2">Client 2</option>
-              </Select>
-          </Box>
-          {isLoading && <div>Loading...</div>}
-          {error ? <HttpError message={error.message} /> : <TransactionTable />}
-        </VStack>
+            <option value="option1">Client 1</option>
+            <option value="option2">Client 2</option>
+          </Select>
+        </Box>
+        {isLoading && <Spinner />}
+        {error ? <HttpError message={error.message} /> : <TransactionTable />}
+      </VStack>
     </>
   );
 }
